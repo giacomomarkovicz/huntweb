34c56c557b8b2013d064b1c0720619f76771eba8
Giácomo Markovicz <giacomocamposv2@hotmail.com>
2019-09-04 18:46:59 -0300

    Inserido Main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/index';
+import Main from './pages/main/index';
 
 import './styles.css';
 
 const App = () => (
   <div className='App'>
     <Header />
+    <Main />
   </div>
 );
 
